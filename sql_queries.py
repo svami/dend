aaf669289667bb59bf079445d11309ee7c18c3bd
svami <azagoskin@griddynamics.com>
2020-05-09 17:22:24 -0400

    Add files via upload

---
+++ sql_queries.py
@@ -0,0 +1,99 @@
+# DROP TABLES
+
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users;"
+song_table_drop = "DROP TABLE IF EXISTS songs;"
+artist_table_drop = "DROP TABLE IF EXISTS artists;"
+time_table_drop = "DROP TABLE IF EXISTS time;"
+
+# CREATE TABLES
+
+songplay_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songplays
+       (songplay_id SERIAL PRIMARY KEY,
+        start_time BIGINT,
+        user_id BIGINT,
+        level CHAR(4),
+        song_id VARCHAR,
+        artist_id VARCHAR,
+        session_id BIGINT,
+        location TEXT,
+        user_agent TEXT);    
+""")
+
+user_table_create = ("""
+    CREATE TABLE IF NOT EXISTS users
+       (user_id BIGINT,
+        first_name VARCHAR,
+        last_name VARCHAR,
+        gender CHAR(1),
+        level CHAR(4)); 
+""")
+
+song_table_create = ("""
+    CREATE TABLE IF NOT EXISTS songs
+       (song_id VARCHAR,
+        title VARCHAR,
+        artist_id VARCHAR,
+        year INT,
+        duration NUMERIC(8,5)); 
+""")
+
+artist_table_create = ("""
+    CREATE TABLE IF NOT EXISTS artists
+       (artist_id VARCHAR,
+        name VARCHAR,
+        location VARCHAR,
+        latitude NUMERIC(10,8),
+        longitude NUMERIC(11,8)); 
+""")
+
+time_table_create = ("""
+    CREATE TABLE IF NOT EXISTS time
+       (start_time TIMESTAMP,
+        hour SMALLINT,
+        day SMALLINT,
+        week SMALLINT,
+        month SMALLINT,
+        year SMALLINT,
+        weekday SMALLINT); 
+""")
+
+# INSERT RECORDS
+
+songplay_table_insert = ("""
+    INSERT INTO songplays (start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
+    VALUES (%s, %s, %s, %s, %s, %s, %s, %s);
+""")
+
+user_table_insert = ("""
+    INSERT INTO users (user_id, first_name, last_name, gender, level) VALUES (%s, %s, %s, %s, %s);
+""")
+
+song_table_insert = ("""
+    INSERT INTO songs (song_id, title, artist_id, year, duration) VALUES (%s, %s, %s, %s, %s);
+""")
+
+artist_table_insert = ("""
+    INSERT INTO artists (artist_id, name, location, latitude, longitude) VALUES (%s, %s, %s, %s, %s);
+""")
+
+
+time_table_insert = ("""
+    INSERT INTO time (start_time, hour, day, week, month, year, weekday) VALUES (%s, %s, %s, %s, %s, %s, %s);
+""")
+
+# FIND SONGS
+
+song_select = ("""
+    SELECT s.song_id, a.artist_id
+    FROM songs AS s
+    JOIN artists AS a
+        ON s.artist_id=a.artist_id
+    WHERE s.title = %s AND a.name = %s AND s.duration = %s;
+""")
+
+# QUERY LISTS
+
+create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
